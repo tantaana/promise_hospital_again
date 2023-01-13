bc2abb89effd0c552ae5ad031a74ad80f8ff2a6e
jahidhasan234031 <jahidhasan234031@mail.com>
2023-01-13 20:51:56 +0600

    'add'

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -3,11 +3,13 @@ import AllGallery from '../Gallery/AllGallery';
 import HomeCarousel from '../HomeCarousel/HomeCarousel';
 import InternationalCenters from '../InternationalCenters/InternationalCenters';
 import MedicalValue from '../MedicalValue/MedicalValue';
+import Overview from '../Overview/Overview';
 
 const Home = () => {
     return (
         <div>
             <HomeCarousel></HomeCarousel>
+            <Overview></Overview>
             <AllGallery></AllGallery>
             <InternationalCenters></InternationalCenters>
             <MedicalValue></MedicalValue>
---
+++ src/Pages/Home/Overview/Overview.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Overview = () => {
+    return (
+        <div className='py-8 px-2   sm:px-64 md:px-64  ' >
+            <div className=' '>
+                <h2 className='text-3xl font-semibold text-center mb-2' style={{ color: '#0038FF' }}>Overview</h2>
+                <div className='text-base'>
+                    <p cl>Thousands of International patients visit Manipal Hospitals in India for medical treatment every year.
+                        The patients travel to our hospitals for sophisticated heart surgery, cancer treatment, organ transplantation,
+                        treatment for infertility, orthopaedic surgery, neurology, treatment for kidney diseases, and general health check-up.
+                        Book an appointment for the best treatment.
+                    </p>
+                    <br />
+                    <p className='text-base'>
+                        Manipal Hospitals is at the forefront of medical tourism in India.
+                        We provide world-class treatment by qualified surgeons and specialists at less than half the cost of that in developed nations.
+                        With a network of more than 28 hospitals across 15+ cities in India, we are the first choice for International patients.
+                        We offer full support and assistance at every step of the treatment right from enquiry to visa assistance and accommodation for
+                        patients from outside India so that they can get quality treatment without any worries.
+                    </p>
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Overview;
