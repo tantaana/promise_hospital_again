7b13cab5d045986c8c5401bb37a9e5564399c349
jahidhasan234031 <jahidhasan234031@mail.com>
2023-01-14 22:34:42 +0600

    again

--- src/Pages/Home/Overview/Overview.js
+++ src/Pages/Home/Overview/Overview.js
@@ -10,14 +10,14 @@ const Overview = () => {
                 <div className=' '>
                     <h2 className='text-3xl font-semibold text-center mb-2' style={{ color: '#0038FF' }}>Overview</h2>
                     <div className='text-base'>
-                        <p cl>Thousands of International patients visit Manipal Hospitals in India for medical treatment every year.
+                        <p cl>Thousands of International patients visit Promise Hospitals in India for medical treatment every year.
                             The patients travel to our hospitals for sophisticated heart surgery, cancer treatment, organ transplantation,
                             treatment for infertility, orthopaedic surgery, neurology, treatment for kidney diseases, and general health check-up.
                             Book an appointment for the best treatment.
                         </p>
                         <br />
                         <p className='text-base'>
-                            Manipal Hospitals is at the forefront of medical tourism in India.
+                            Promise Hospitals is at the forefront of medical tourism in India.
                             We provide world-class treatment by qualified surgeons and specialists at less than half the cost of that in developed nations.
                             With a network of more than 28 hospitals across 15+ cities in India, we are the first choice for International patients.
                             We offer full support and assistance at every step of the treatment right from enquiry to visa assistance and accommodation for
