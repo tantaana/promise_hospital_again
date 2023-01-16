bf821b5c41ee3a03346651ca994a74c62d4a36b4
jahidhasan234031 <jahidhasan234031@mail.com>
2023-01-16 18:26:37 +0600

    done

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,15 +5,16 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBx_oiktnI8G0145bRpVy6jTrFAsAEm5NU",
-    authDomain: "promise-hospital.firebaseapp.com",
-    projectId: "promise-hospital",
-    storageBucket: "promise-hospital.appspot.com",
-    messagingSenderId: "113926730675",
-    appId: "1:113926730675:web:2e9e5807cfbfe57b9fd1c6"
+    apiKey: "AIzaSyBWPwYqVOnY0dxQQW3VEV9elTRIouf7RO8",
+    authDomain: "promise-hospital-5e53f.firebaseapp.com",
+    projectId: "promise-hospital-5e53f",
+    storageBucket: "promise-hospital-5e53f.appspot.com",
+    messagingSenderId: "849865260919",
+    appId: "1:849865260919:web:e008d89495323397902b72"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export default app;
+
--- src/index.js
+++ src/index.js
@@ -5,17 +5,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+
 const queryClient = new QueryClient()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
+  <QueryClientProvider client={queryClient}>
 
-      <App />
+    <App />
 
-    </QueryClientProvider>
-  </React.StrictMode>
+  </QueryClientProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
