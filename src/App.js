162e4f4c09f4e9b8cc488fb9e52856ff398e5a56
jahidhasan234031 <jahidhasan234031@mail.com>
2023-01-16 16:46:32 +0600

    done

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './Routes/Routes/Routes';
-import { Toast } from 'react-toastify/dist/components';
 import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <div className="App">
       <RouterProvider router={router}></RouterProvider>
-      <Toast></Toast>
       <Toaster></Toaster>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import AuthProvider from './Context/Authprovider';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient()
@@ -12,9 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
+
+      <App />
+
     </QueryClientProvider>
   </React.StrictMode>
 );
