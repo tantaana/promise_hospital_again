6ed777421079088b5e70a3142031981ae84c7f4d
jahidhasan234031 <jahidhasan234031@mail.com>
2023-01-16 15:29:26 +0600

    good

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './Routes/Routes/Routes';
+import { Toast } from 'react-toastify/dist/components';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <div className="App">
       <RouterProvider router={router}></RouterProvider>
+      <Toast></Toast>
+      <Toaster></Toaster>
     </div>
   );
 }
