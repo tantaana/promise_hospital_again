b5195084a3e73ee4b6010ff3ad9b370daae33df3
Fouzia Tabassum <tabassumfouzia13@gmail.com>
2023-02-28 17:12:24 +0600

    final data

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -7,6 +7,7 @@ import HomeCarousel from '../HomeCarousel/HomeCarousel';
 import InternationalCenters from '../InternationalCenters/InternationalCenters';
 import MedicalValue from '../MedicalValue/MedicalValue';
 import Overview from '../Overview/Overview';
+import DoctorHome from './Doctor/DoctorHome';
 
 const Home = () => {
     AllTitle('Home')
@@ -14,7 +15,9 @@ const Home = () => {
         <div>
             <HomeCarousel></HomeCarousel>
             <Overview></Overview>
-            <AllGallery></AllGallery>
+          
+            <AllGallery></AllGallery> 
+            <DoctorHome></DoctorHome>
             <InternationalCenters></InternationalCenters>
             <MedicalValue></MedicalValue>
             <Book></Book>
